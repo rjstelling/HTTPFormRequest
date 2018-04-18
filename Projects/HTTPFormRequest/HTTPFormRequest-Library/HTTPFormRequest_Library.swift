6f12bbd0c3dde603f190fb9361db70a6b42ece13
Richard Stelling <rich@lionheartapplications.com>
2018-04-18 17:58:58 +0100

    Added Static Lib target

---
+++ Projects/HTTPFormRequest/HTTPFormRequest-Library/HTTPFormRequest_Library.swift
@@ -0,0 +1,11 @@
+//
+//  HTTPFormRequest_Library.swift
+//  HTTPFormRequest-Library
+//
+//  Created by Richard Stelling on 18/04/2018.
+//  Copyright © 2018 Richard Stelling. All rights reserved.
+//
+
+class HTTPFormRequest_Library {
+
+}
